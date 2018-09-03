c945c939105fee1fae5c5973f45469e038f2fce6
Adiemus994 <amek994@gmail.com>
2018-09-03 16:53:40 +0200

    Przeniesiono funckję niszczenia platform jak i licznika punktów do osobnego skryptu Helixplatform, który teraz odpowiada za obsługę platform i dalej wyświetlanie punktów. Uporządkowanie skryptu BallController ze zbędnych części kodu

--- Assets/BallControler.cs
+++ Assets/BallControler.cs
@@ -11,21 +11,13 @@ public class BallControler : MonoBehaviour
     public float GravityScale = 1.0f;
 
     private Rigidbody ball;
-    private IList<GameObject> platforms;
     private bool isForce = false;
-    private float points=0;
+    
 
     private void Start()
     {
         ball = GetComponent<Rigidbody>();
-        //helixall = GetComponent<GameObject>();
-
-        //ball.useGravity = false;
-
-        // funkcje lambda / funkcje anonimowe
-        platforms = GameObject.FindGameObjectsWithTag("Helix")
-            .OrderBy(x => x.transform.position.y)
-            .ToList();
+      
     }
 
     private void FixedUpdate()
@@ -33,21 +25,6 @@ public class BallControler : MonoBehaviour
          Vector3 gravity = GlobalGravity * GravityScale * Vector3.up;
          ball.AddForce(gravity, ForceMode.Acceleration);
 
-        if (platforms.Count > 1)
-        {
-            foreach (var platform in platforms.Skip(1).ToList())
-            {
-                if (ball.transform.position.y + 7.20f < platform.transform.position.y)
-                {
-                    platform.SetActive(false);
-                    platforms.Remove(platform);
-                    points++;
-                    Debug.Log(string.Format("Usunieto:{0} ", points));
-                    // Debug.Log(string.Format("Ball{0}\t Platfroms: {1}\t localposition ball{2}\t localposition{3}",ball.transform.position.y, platform.transform.position.y, ball.transform.localPosition.y, platform.transform.localPosition.y));
-
-                }
-            }
-        }
     }
 
      void force()
--- Assets/Helixplatform.cs
+++ Assets/Helixplatform.cs
@@ -1,16 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class Helixplatform : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Rigidbody ball;
+    private IList<GameObject> platforms;
+    private float points = 0;
+
+    // Use this for initialization
+    void Start () {
+	    platforms = GameObject.FindGameObjectsWithTag("Helix")
+	        .OrderBy(x => x.transform.position.y)
+	        .ToList();
+    }
+
+    private void FixedUpdate()
+    {
+        if (platforms.Count > 1)
+        {
+            foreach (var platform in platforms.Skip(1).ToList())
+            {
+                if (ball.transform.position.y + 7.20f < platform.transform.position.y)
+                {
+                    platform.SetActive(false);
+                    platforms.Remove(platform);
+                    points++;
+                    Debug.Log(string.Format("Usunieto:{0} ", points));
+                    // Debug.Log(string.Format("Ball{0}\t Platfroms: {1}\t localposition ball{2}\t localposition{3}",ball.transform.position.y, platform.transform.position.y, ball.transform.localPosition.y, platform.transform.localPosition.y));
+
+                }
+            }
+        }
+    }
 }
